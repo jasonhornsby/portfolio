7621723823f360e23bb59df9268db73946761964
Jason Hornsby <1996jasonhornsby@gmail.com>
2025-11-28 14:48:59 +1100

    Added error handling page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+	import { toast } from "svelte-sonner";
+
+    onMount(() => {
+        toast.info('Could not find that page. Back home we go!')
+        goto('/')
+    })
+</script>
